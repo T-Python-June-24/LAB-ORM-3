b252bb9ea5139c49cd2a15a7106d928a7849221c
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-01 11:39:48 +0300

    Implement paginator

--- InventoryPlus/main/views.py
+++ InventoryPlus/main/views.py
@@ -4,7 +4,8 @@ from product.models import Product
 from supplier.models import Supplier
 from inventory.models import Inventory
 from django.db.models import Sum
-
+from django.core.paginator import Paginator
+from django.contrib import messages
 
 
 def dashboard_view(request:HttpRequest):
@@ -12,19 +13,21 @@ def dashboard_view(request:HttpRequest):
     inventory = Inventory.objects.all()
     total_products = Product.objects.count()
     total_suppliers = Supplier.objects.count()
-    total_inventory_items = Inventory.objects.count()
-    products_with_quantity = Product.objects.annotate(
-        total_quantity=Sum('inventory__quantity')
-    )
+    total_inventory_items = Inventory.objects.count() # count the number of products using 
+    products_with_quantity = Product.objects.annotate( total_quantity=Sum('inventory__quantity'))
+
+    page_number = request.GET.get("page", 1)
+    paginator = Paginator(products_with_quantity, 2)
+    product_page = paginator.get_page(page_number)
+
 
 
     context = {
         'total_products': total_products,
         'total_suppliers': total_suppliers,
         'total_inventory_items': total_inventory_items,
-        'products_with_quantity': products_with_quantity,
-        'products': product,
-        'inventories':inventory
+        'products': product_page,
+        'inventories':inventory,
     }
 
     return render(request, 'index.html', context)
