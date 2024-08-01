1beb562e998adc792f0f48f051356eff8bac9dae
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-01 12:17:03 +0300

    implement aggrgate

--- InventoryPlus/main/views.py
+++ InventoryPlus/main/views.py
@@ -14,6 +14,8 @@ def dashboard_view(request:HttpRequest):
     total_products = Product.objects.count()
     total_suppliers = Supplier.objects.count()
     total_inventory_items = Inventory.objects.count() # count the number of products using 
+    total_amount = Product.objects.aggregate(total=Sum('price'))['total'] or 0 #implement aggregate total price of product
+    print(total_amount)
     products_with_quantity = Product.objects.annotate( total_quantity=Sum('inventory__quantity')) # Combining multiple aggregations with annotate() 
 
     page_number = request.GET.get("page", 1)
@@ -26,6 +28,7 @@ def dashboard_view(request:HttpRequest):
         'total_products': total_products,
         'total_suppliers': total_suppliers,
         'total_inventory_items': total_inventory_items,
+        'total_product_amount': total_amount,
         'products': product_page,
         'inventories':inventory,
     }
