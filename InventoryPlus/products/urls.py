7b221abd46a8f5817cbb2597ca19add2ea58e59e
D. F <ifonei@hotmail.com>
2024-08-01 12:19:34 +0300

    test

--- InventoryPlus/products/urls.py
+++ InventoryPlus/products/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("products/<type>/<product_param>/", views.all_products_view, name="all_products_view"),
     path('product_detail/<product_id>/', views.product_detail_view, name='product_detail_view'),
     path("export_products_csv/", views.export_products_csv_view, name="export_products_csv_view"),
-    path("csv_products/", views.import_products_csv_view, name="import_products_csv_view"),
+    path("csv_products/", views.import_products_csv_view, name="import_products_csv_view"), #dc;f;c
 
 ]
 
